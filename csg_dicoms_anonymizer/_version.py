be1a1fee407d9152602cb34220117fc0adf34cd9
Stephen L <lrq3000@gmail.com>
2018-02-17 15:43:20 +0100

    update version to be able to push readme to pypi (immutability...)
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- csg_dicoms_anonymizer/_version.py
+++ csg_dicoms_anonymizer/_version.py
@@ -3,7 +3,7 @@
 __all__ = ["__version__"]
 
 # major, minor, patch, -extra
-version_info = 1, 4, 1
+version_info = 1, 4, 2
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
